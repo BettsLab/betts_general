5bc5f2f6c33b4ec04d0d275343e81f33306f28c6
wuv <wuv21@uw.edu>
2021-01-25 10:28:55 -0500

    added py script to parse pubmed csv to paste into weebly website

---
+++ lab_website/publication_parser.py
@@ -0,0 +1,17 @@
+import csv
+
+n_studies = 25
+with open("csv-BettsMRAut-set.csv", "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter = ",")
+    line_n = 0
+
+    for row in reader:
+        if line_n != 0:
+            print(row[1]) #title
+            print(row[2] + " " + row[3]) #authors + citation
+            print("")
+
+            if line_n + 1 >= n_studies:
+                break
+
+        line_n += 1
